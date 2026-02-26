982d043b8b3fb66612e40f2fbfeb42c591b45bdb
Hashaam Alvi <hashaam.imran.alvi@gmail.com>
2026-02-26 10:22:55 +0500

    Railway, port is adjust

--- Server/index.js
+++ Server/index.js
@@ -14,7 +14,7 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-const port =process.env.port  || 8081;
+const port =process.env.PORT  || 8081;
 
 app.listen(port, () => {
   console.log("Server started on 8081");
